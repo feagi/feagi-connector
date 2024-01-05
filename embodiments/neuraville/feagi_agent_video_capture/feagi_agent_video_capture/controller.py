8ccda29003a4af4d9634e05f2904e7806ca6495e
kakcalu13 <kevin.a.araujo@gmail.com>
2024-01-05 10:47:00 -0500

    uncomment vision part

--- embodiments/neuraville/feagi_agent_video_capture/feagi_agent_video_capture/controller.py
+++ embodiments/neuraville/feagi_agent_video_capture/feagi_agent_video_capture/controller.py
@@ -130,20 +130,21 @@ def main(feagi_auth_url, feagi_settings, agent_settings, capabilities, message_t
                                        camera_data['vision'],), daemon=True).start()
     while True:
         try:
-            # if camera_data['vision'] is not None:
-            #     raw_frame = camera_data['vision']
-            # if 'camera' in default_capabilities:
-            #     if default_capabilities['camera']['blink'] != []:
-            #         raw_frame = default_capabilities['camera']['blink']
-            # previous_frame_data, rgb, default_capabilities = retina.update_region_split_downsize(
-            #     raw_frame,
-            #     default_capabilities,
-            #     size_list,
-            #     previous_frame_data,
-            #     rgb, capabilities)
-            # default_capabilities['camera']['blink'] = []
-            # message_to_feagi = pns.generate_feagi_data(rgb, msg_counter, datetime.now(),
-            #                                            message_to_feagi)
+            if camera_data['vision'] is not None:
+                raw_frame = camera_data['vision']
+            if 'camera' in default_capabilities:
+                if default_capabilities['camera']['blink'] != []:
+                    raw_frame = default_capabilities['camera']['blink']
+            previous_frame_data, rgb, default_capabilities = retina.update_region_split_downsize(
+                raw_frame,
+                default_capabilities,
+                size_list,
+                previous_frame_data,
+                rgb, capabilities)
+            default_capabilities['camera']['blink'] = []
+            message_to_feagi = pns.generate_feagi_data(rgb, msg_counter, datetime.now(),
+                                                       message_to_feagi)
+            # print(default_capabilities['camera']['gaze_control'][0])
             sleep(feagi_settings['feagi_burst_speed']) #bottleneck
             pns.signals_to_feagi(message_to_feagi, feagi_ipu_channel, agent_settings)
             message_to_feagi.clear()
