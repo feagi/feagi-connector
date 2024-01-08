f8394af31c8480d1302af85c0a398a9af56672a0
kakcalu13 <kevin.a.araujo@gmail.com>
2024-01-08 14:24:14 -0500

    added library to make async smooth

--- feagi_agent_core/feagi_agent/router.py
+++ feagi_agent_core/feagi_agent/router.py
@@ -16,6 +16,7 @@ limitations under the License.
 ==============================================================================
 """
 import zmq
+import zmq.asyncio
 import json
 import socket
 import pickle
