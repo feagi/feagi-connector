bd31c41995d25e470147fe68f52341ad29513d45
kakcalu13 <kevin.a.araujo@gmail.com>
2024-01-16 15:56:01 -0500

    added motor generate power

--- feagi_agent_core/feagi_agent/actuators.py
+++ feagi_agent_core/feagi_agent/actuators.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from feagi_agent import feagi_interface as feagi
+from feagi_agent import pns_gateway as pns
 
 
 def obtain_opu_data(device_list, message_from_feagi):
@@ -12,3 +13,11 @@ def obtain_opu_data(device_list, message_from_feagi):
                     opu_signal_dict[i] = {}
                 opu_signal_dict[i][x] = opu_data[i][x]
     return opu_signal_dict
+
+
+def motor_generate_power(power_maximum, feagi_power):
+    z_depth = pns.full_list_dimension['motor_opu'][6]
+    if z_depth == 1:
+        return power_maximum * (feagi_power/100)
+    else:
+        return (feagi_power / z_depth) * power_maximum
