a9befe25d5dbe706c4d110622932e3709e1851f2
kakcalu13 <kevin.a.araujo@gmail.com>
2024-02-01 18:03:46 -0500

    tweaked to make it scalable

--- embodiments/arduino/pyfirmata/controller.py
+++ embodiments/arduino/pyfirmata/controller.py
@@ -1,6 +1,5 @@
 from time import sleep
 import time
-import json
 import threading
 from configuration import *
 from feagi_agent import feagi_interface as feagi
@@ -9,16 +8,19 @@ from feagi_agent.version import __version__
 from feagi_agent import actuators
 from pyfirmata import Arduino, SERVO
 
+servo_status = dict()
+motor_status = dict()
+
 
 def action(obtained_data):
     recieve_servo_data = actuators.get_servo_data(obtained_data)
     if recieve_servo_data:
         # Do some custom work with servo data as well
         for id in recieve_servo_data:
-            print(recieve_servo_data)
             servo_power = actuators.servo_generate_power(180, recieve_servo_data[id], id)
-            print("power: ", servo_power)
+            
             servo.write(servo_power)
+
 
 if __name__ == "__main__":
     # # FEAGI REACHABLE CHECKER # #
@@ -52,24 +54,21 @@ if __name__ == "__main__":
     # Specify the serial port where your Arduino is connected (e.g., 'COM3' on Windows or '/dev/ttyUSB0' on Linux)
     feagi_settings['feagi_burst_speed'] = float(runtime_data["feagi_state"]['burst_duration'])
     threading.Thread(target=pns.feagi_listener, args=(feagi_opu_channel,), daemon=True).start()
-    port = 'COM5'
+    port = capabilities['arduino']['port']  # Don't change this
     board = Arduino(port)
-    print("inital;izing the board")
     time.sleep(5)
     servo_pin = 13
     servo = board.get_pin('d:{0}:s'.format(servo_pin))
     print(servo)
-    sleep(5)
     servo.write(90)
-
 
     while True:
         message_from_feagi = pns.message_from_feagi
         if message_from_feagi:
             # Fetch data such as motor, servo, etc and pass to a function (you make ur own action.
             pns.check_genome_status_no_vision(message_from_feagi)
-            feagi_settings['feagi_burst_speed'] = pns.check_refresh_rate(message_from_feagi,
-                                                                         feagi_settings['feagi_burst_speed'])
+            feagi_settings['feagi_burst_speed'] = \
+                pns.check_refresh_rate(message_from_feagi, feagi_settings['feagi_burst_speed'])
             obtained_signals = pns.obtain_opu_data(message_from_feagi)
             action(obtained_signals)
         sleep(feagi_settings['feagi_burst_speed'])
