09d92e6b989d1d58cde94bb50bccac2829fc5de0
kakcalu13 <kevin.a.araujo@gmail.com>
2024-02-01 13:29:52 -0500

    updated controller with new config

--- embodiments/raspberry_pi/controller.py
+++ embodiments/raspberry_pi/controller.py
@@ -1,5 +1,4 @@
 from time import sleep
-import time
 import threading
 from configuration import *
 from feagi_agent import feagi_interface as feagi
@@ -12,6 +11,7 @@ import raspberry_PI_library as rpi
 def action(obtained_data):
     recieve_gpio_data = actuators.get_gpio_data(obtained_data)
     if recieve_gpio_data:
+        print("here: ", recieve_gpio_data)
         for i in recieve_gpio_data:
             rpi.power_pin(i)
     else:
@@ -40,18 +40,17 @@ if __name__ == "__main__":
     feagi_settings['feagi_burst_speed'] = float(runtime_data["feagi_state"]['burst_duration'])
     threading.Thread(target=pns.feagi_listener, args=(feagi_opu_channel,), daemon=True).start()
 
+    rpi.configured_board_by_config(capabilities) # pass your config setting to this
+
     while True:
         try:
             message_from_feagi = pns.message_from_feagi
             if message_from_feagi:
-                # Fetch data such as motor, servo, etc and pass to a function (you make ur own action.
                 pns.check_genome_status_no_vision(message_from_feagi)
-                feagi_settings['feagi_burst_speed'] = pns.check_refresh_rate(message_from_feagi,
-                                                                             feagi_settings[
-                                                                                 'feagi_burst_speed'])
+                feagi_settings['feagi_burst_speed'] = \
+                    pns.check_refresh_rate(message_from_feagi, feagi_settings['feagi_burst_speed'])
                 obtained_signals = pns.obtain_opu_data(message_from_feagi)
                 action(obtained_signals)
             sleep(feagi_settings['feagi_burst_speed'])
         except KeyboardInterrupt:
             rpi.clear_gpio()
-
