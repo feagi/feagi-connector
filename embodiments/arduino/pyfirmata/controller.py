91f562f89ed58c734b51423706653c8ad3b3d3f5
kakcalu13 <kevin.a.araujo@gmail.com>
2024-02-02 01:47:34 -0500

    scalable with any type of pin, completed servo and WIP on motor

--- embodiments/arduino/pyfirmata/controller.py
+++ embodiments/arduino/pyfirmata/controller.py
@@ -2,6 +2,7 @@ from time import sleep
 import time
 import threading
 from configuration import *
+from collections import deque
 from feagi_agent import feagi_interface as feagi
 from feagi_agent import pns_gateway as pns
 from feagi_agent.version import __version__
@@ -10,16 +11,45 @@ from pyfirmata import Arduino, SERVO
 
 servo_status = dict()
 motor_status = dict()
+pin_board = dict()
+rolling_window = {}
+rolling_window_len = capabilities['motor']['rolling_window_len']
+motor_count = capabilities['motor']['count']
 
+
+
+def list_all_pins(board):
+    all_pins = dict()
+    for pin in board._layout['digital']:
+        if pin not in [0, 1]: # 0 and 1 are not available for output.
+            all_pins[pin] = "" # initalize empty key
+    for pin in all_pins:
+        current = pin-2 # Due to serial communcations port
+        pin_board[current] = board.get_pin('d:{0}:s'.format(int(pin)))
 
 def action(obtained_data):
-    recieve_servo_data = actuators.get_servo_data(obtained_data)
+    recieve_servo_data = actuators.get_servo_data(obtained_data, True)
+    recieve_motor_data = actuators.get_motor_data(obtained_data,
+                                                  capabilities['motor']['power_amount'],
+                                                  capabilities['motor']['count'], rolling_window)
     if recieve_servo_data:
         # Do some custom work with servo data as well
         for id in recieve_servo_data:
             servo_power = actuators.servo_generate_power(180, recieve_servo_data[id], id)
-            
-            servo.write(servo_power)
+            if id in motor_status:
+                del motor_status[id]
+            if id not in servo_status:
+                servo_status[id] = actuators.servo_keep_boundaries(servo_power)
+                pin_board[id].write(servo_status[id])
+            else:
+                servo_status[id] += servo_power / 100
+                servo_status[id] = actuators.servo_keep_boundaries(servo_status[id])
+                pin_board[id].write(servo_status[id])
+        print("servo status: ", servo_status, " and motor status: ", motor_status)
+    if recieve_motor_data:
+        for i in recieve_motor_data:
+            pin_board[i].write(1)
+
 
 
 if __name__ == "__main__":
@@ -57,10 +87,14 @@ if __name__ == "__main__":
     port = capabilities['arduino']['port']  # Don't change this
     board = Arduino(port)
     time.sleep(5)
-    servo_pin = 13
-    servo = board.get_pin('d:{0}:s'.format(servo_pin))
-    print(servo)
-    servo.write(90)
+    # servo_pin = 13
+    # servo = board.get_pin('d:{0}:s'.format(servo_pin))
+    # print(servo)
+    # servo.write(90)
+    list_all_pins(board)
+    # Initialize rolling window for each motor
+    for motor_id in range(motor_count):
+        rolling_window[motor_id] = deque([0] * rolling_window_len)
 
     while True:
         message_from_feagi = pns.message_from_feagi
