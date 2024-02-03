40ffeff09fee9d7448a23780565d37a94551b79f
kakcalu13 <kevin.a.araujo@gmail.com>
2024-02-03 15:34:57 -0500

    completed input, output

--- embodiments/arduino/pyfirmata/controller.py
+++ embodiments/arduino/pyfirmata/controller.py
@@ -10,6 +10,7 @@ from feagi_agent import feagi_interface as feagi
 from feagi_agent import pns_gateway as pns
 from feagi_agent.version import __version__
 from feagi_agent import actuators
+from feagi_agent import sensors
 from pyfirmata import Arduino, SERVO, util
 
 servo_status = dict()
@@ -21,6 +22,7 @@ rolling_window = {}
 rolling_window_len = capabilities['motor']['rolling_window_len']
 motor_count = capabilities['motor']['count']
 output_track = list()
+input_track = list()
 
 
 def list_all_pins(board):
@@ -29,7 +31,7 @@ def list_all_pins(board):
         if pin not in [0, 1]:  # 0 and 1 are not available for output.
             all_pins[pin] = ""  # initalize empty key
     for pin in all_pins:
-        current = pin - 2  # Due to serial communcations port
+        current = pin  # Due to serial communcations port
         pin_board[current] = board.get_pin('d:{0}:s'.format(int(pin)))
         pin_mode[current] = 4
 
@@ -51,12 +53,24 @@ def set_pin_mode(pin, mode, id):
 def action(obtained_data):
     recieve_servo_data = actuators.get_servo_data(obtained_data, True)
     recieve_gpio_data = actuators.get_gpio_data(obtained_data)
+    check_input_request = actuators.check_convert_gpio_to_input(obtained_data)
+    if check_input_request:
+        for id in check_input_request:
+            if id in pin_mode:
+                if pin_mode[int(id)] != 0:
+                    set_pin_mode(pin_board[int(id)], 0, id)
+                    input_track.append(id)
     if recieve_gpio_data:
         for i in recieve_gpio_data:
-            if pin_mode[int(i)] != 1:
-                set_pin_mode(pin_board[int(i)], 1, i)
-            pin_board[int(i)].write(1)
-            output_track.append(int(i))  # Tracking whatever used digital
+            if i in pin_mode:
+                if pin_mode[int(i)] != 1:
+                    set_pin_mode(pin_board[int(i)], 1, i)
+                    if int(i) in input_track:
+                        input_track.remove(int(i))
+                pin_board[int(i)].write(1)
+                output_track.append(int(i))  # Tracking whatever used digital
+            else:
+                print("pin: ", i, " is not configured. Select another pin please.")
     else:
         if output_track:
             pin_board[output_track[0]].write(0)
@@ -65,21 +79,22 @@ def action(obtained_data):
     if recieve_servo_data:
         # Do some custom work with servo data as well
         for id in recieve_servo_data:
-            if pin_mode[int(id)] != 4:
-                print("reset your board to use the servo again after you updated pin: ", id)
-                set_pin_mode(pin_board[int(id)], pyfirmata.OUTPUT, id)
-            servo_power = actuators.servo_generate_power(180, recieve_servo_data[id], id)
-            if id in motor_status:
-                del motor_status[id]
-            if id not in servo_status:
-                servo_status[id] = actuators.servo_keep_boundaries(servo_power)
-                pin_board[id].write(servo_status[id])
+            if id in pin_mode:
+                if pin_mode[int(id)] != 4:
+                    print("reset your board to use the servo again after you updated pin: ", id)
+                    set_pin_mode(pin_board[int(id)], pyfirmata.OUTPUT, id)
+                servo_power = actuators.servo_generate_power(180, recieve_servo_data[id], id)
+                if id in motor_status:
+                    del motor_status[id]
+                if id not in servo_status:
+                    servo_status[id] = actuators.servo_keep_boundaries(servo_power)
+                    pin_board[id].write(servo_status[id])
+                else:
+                    servo_status[id] += servo_power / 100
+                    servo_status[id] = actuators.servo_keep_boundaries(servo_status[id])
+                    pin_board[id].write(servo_status[id])
             else:
-                servo_status[id] += servo_power / 100
-                servo_status[id] = actuators.servo_keep_boundaries(servo_status[id])
-                pin_board[id].write(servo_status[id])
-        print("servo status: ", servo_status, " and motor status: ", motor_status)
-
+                print("pin: ", id, " is not configured. Select another pin please.")
 
 if __name__ == "__main__":
     # # FEAGI REACHABLE CHECKER # #
@@ -110,14 +125,15 @@ if __name__ == "__main__":
         feagi.connect_to_feagi(feagi_settings, runtime_data, agent_settings, capabilities,
                                __version__)
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    # Specify the serial port where your Arduino is connected (e.g., 'COM3' on Windows or '/dev/ttyUSB0' on Linux)
+    # Specify the serial port where your Arduino is connected (e.g., 'COM3' on Windows or
+    # '/dev/ttyUSB0' on Linux)
     feagi_settings['feagi_burst_speed'] = float(runtime_data["feagi_state"]['burst_duration'])
     threading.Thread(target=pns.feagi_listener, args=(feagi_opu_channel,), daemon=True).start()
     port = capabilities['arduino']['port']  # Don't change this
     board = Arduino(port)
-    it = util.Iterator(board)
+    it = util.Iterator(board)  # for Analog or Input
     it.start()
-    list_all_analog_pins(board)
+    # list_all_analog_pins(board) # Temporarily pause analog section
     time.sleep(5)
     list_all_pins(board)
     # Initialize rolling window for each motor
@@ -125,7 +141,6 @@ if __name__ == "__main__":
         rolling_window[motor_id] = deque([0] * rolling_window_len)
 
     while True:
-        print(analog_pin_board[0].read())
         message_from_feagi = pns.message_from_feagi
         if message_from_feagi:
             # Fetch data such as motor, servo, etc and pass to a function (you make ur own action.
@@ -134,4 +149,15 @@ if __name__ == "__main__":
                 pns.check_refresh_rate(message_from_feagi, feagi_settings['feagi_burst_speed'])
             obtained_signals = pns.obtain_opu_data(message_from_feagi)
             action(obtained_signals)
+        if input_track:
+            create_generic_input_dict = dict()
+            create_generic_input_dict['i_gpio'] = dict()
+            for pin in input_track:
+                obtain_data = pin_board[pin].read()
+                if obtain_data:
+                    location_string = str(pin) + "-0-0"
+                    create_generic_input_dict['i_gpio'][location_string] = 100
+            message_to_feagi = sensors.add_generic_input_to_feagi_data(
+                create_generic_input_dict, message_to_feagi)
+            pns.signals_to_feagi(message_to_feagi, feagi_ipu_channel, agent_settings)
         sleep(feagi_settings['feagi_burst_speed'])
