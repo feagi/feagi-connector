ab914a9e0eebfe1d7f1a62e1ac192bbb8d94e804
kakcalu13 <kevin.a.araujo@gmail.com>
2024-04-05 17:13:47 -0400

    remove magic link from function

--- embodiments/digital_dream_labs/cozmo_1.0/controller.py
+++ embodiments/digital_dream_labs/cozmo_1.0/controller.py
@@ -263,18 +263,18 @@ def action(obtained_data, arms_angle, head_angle):
 
 
 if __name__ == '__main__':
-    magic_link, feagi_settings, configuration, agent_settings, default_capabilities, \
-    message_to_feagi, capabilities = FEAGI.build_up_from_configuration()
-
-
-
-    # # FEAGI REACHABLE CHECKER COMPLETED # #
+    config = FEAGI.build_up_from_configuration()
+    feagi_settings = config['feagi_settings'].copy()
+    agent_settings = config['agent_settings'].copy()
+    default_capabilities = config['default_capabilities'].copy()
+    message_to_feagi = config['message_to_feagi'].copy()
+    capabilities = config['capabilities'].copy()
 
     # # # FEAGI registration # # # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     # - - - - - - - - - - - - - - - - - - #
     feagi_settings, runtime_data, api_address, feagi_ipu_channel, feagi_opu_channel = \
         FEAGI.connect_to_feagi(feagi_settings, runtime_data, agent_settings, capabilities,
-                               __version__, magic_link=magic_link)
+                               __version__)
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     face_selected = deque()
     eye_one_location = deque()
@@ -386,7 +386,7 @@ if __name__ == '__main__':
                                                              message_to_feagi)
             message_to_feagi = sensors.add_battery_to_feagi_data(battery, message_to_feagi)
             sleep(feagi_settings['feagi_burst_speed'])  # bottleneck
-            if magic_link == '':
+            if 'magic_link' not in feagi_settings:
                 pns.signals_to_feagi(message_to_feagi, feagi_ipu_channel, agent_settings)
             else:
                 router.websocket_send(message_to_feagi)
