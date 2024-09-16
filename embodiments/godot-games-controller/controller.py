917b55416cbaaa844ecc48063ed2a4e5439db645
kakcalu13 <kevin.a.araujo@gmail.com>
2024-09-16 17:40:24 -0400

    updated to send json instead of string dict

--- embodiments/godot-games-controller/controller.py
+++ embodiments/godot-games-controller/controller.py
@@ -1,17 +1,18 @@
-#!/usr/bin/env python
-"""
-Copyright 2016-present The FEAGI Authors. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================
-"""
+#
+# Copyright 2016-Present Neuraville Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 import os
 import json
 import zlib
@@ -130,7 +131,8 @@ async def bridge_to_godot():
                             stored_value = ws.pop()
                             ws.clear()
                             ws.append(stored_value)
-                    await ws_operation[0].send(str(ws[0]))
+                    json_data = json.dumps(ws[0])
+                    await ws_operation[0].send(json_data)
                     ws.pop()
                 if "stimulation_period" in runtime_data:
                     sleep(runtime_data["stimulation_period"])
