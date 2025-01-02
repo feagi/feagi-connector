211ac482a069448cfb85e65cec939afec9c41693
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2025-01-02 12:01:30 -0500

    pending amalgamation

--- embodiments/godot-bridge/bridge_godot_python.py
+++ embodiments/godot-bridge/bridge_godot_python.py
@@ -95,7 +95,9 @@ def main(feagi_settings, runtime_data, capabilities):
             processed_one_frame_dict["status"]["genome_availability"] = one_frame.get("genome_availability")
             processed_one_frame_dict["status"]["genome_validity"] = one_frame.get("genome_validity")
             processed_one_frame_dict["status"]["brain_readiness"] = one_frame.get("brain_readiness")
-            processed_one_frame_dict["status"]["amalgamation_pending"] = one_frame.get("amalgamation_pending", False)
+            pending_amalgamation = one_frame.get("amalgamation_pending")
+            if pending_amalgamation:
+                processed_one_frame_dict["status"]["amalgamation_pending"] = True
             send_to_BV_queue.append(json.dumps(processed_one_frame_dict))
         # If queue_of_recieve_godot_data has a data, it will obtain the latest then pop it for
         # the next data.
