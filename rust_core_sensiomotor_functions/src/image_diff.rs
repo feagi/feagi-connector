5cc78d984fa2c4e1954c197d812731acc41d43c4
Amir Rasteg <amir.rasteg@protonmail.com>
2025-04-17 12:26:53 -0400

    better exception handling

--- rust_core_sensiomotor_functions/src/image_diff.rs
+++ rust_core_sensiomotor_functions/src/image_diff.rs
@@ -25,26 +25,37 @@ impl ImageDiff {
         Self::new(image_width, image_height, color_depth)
     }
 
-    pub fn get_new_delta_from_new_frame(&mut self, new_image_frame: Array3<u8>) -> Array3<u8> {
-        assert_eq!(
-            new_image_frame.shape(),
-            self.latest.shape(),
-            "Resolution mismatch!"
-        );
+    /// Returns the shape of the internal array (color_depth, height, width)
+    pub fn shape(&self) -> (usize, usize, usize) {
+        let shape = self.latest.shape();
+        (shape[0], shape[1], shape[2])
+    }
 
+    pub fn get_new_delta_from_new_frame(&mut self, new_image_frame: Array3<u8>) -> Result<Array3<u8>, String> {
+        // Shape check for sanity
+        if new_image_frame.shape() != self.latest.shape() {
+            return Err(format!(
+                "Resolution mismatch! Expected {:?}, got {:?}",
+                self.latest.shape(),
+                new_image_frame.shape()
+            ));
+        }
+
+        // TODO we will need to use a different comparison algorithm
         let mut diff = Array3::zeros(self.latest.dim());
 
-        // In place subtraction
+        // In place subtraction with saturating subtraction to avoid overflow
         Zip::from(&mut diff)
             .and(&new_image_frame)
             .and(&self.latest)
             .for_each(|d, &new, &old| {
-                *d = new - old;
+                // Use saturating_sub to prevent overflow
+                *d = new.saturating_sub(old);
             });
 
         // In place replacement
         self.previous = std::mem::replace(&mut self.latest, new_image_frame);
 
-        diff
+        Ok(diff)
     }
 }
